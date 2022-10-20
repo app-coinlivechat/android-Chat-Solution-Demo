f8cef3de59ad61a780a581ef4dde12dc3f14d775
etgkqo <etgkqo@gmail.com>
2022-10-20 12:40:03 +0900

    debug 여부 필드 추가

--- app/src/main/java/com/coinlive/chat/CoinliveChat.kt
+++ app/src/main/java/com/coinlive/chat/CoinliveChat.kt
@@ -1,4 +1,15 @@
 package com.coinlive.chat
 
-class CoinliveChat {
+object CoinliveChat {
+    /**
+     * 테스트 환경을 위해 현재 build mode를 설정합니다.
+     * 디폴트 값은 true 입니다.
+     */
+    var isDebug: Boolean = true
+        get() = field
+        set(value) {
+            field = value
+        }
+
+
 }
