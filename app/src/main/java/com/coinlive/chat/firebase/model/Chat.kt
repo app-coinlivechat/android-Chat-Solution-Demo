2d7f3623cce0db2327a969be4f388009e7d3e7e5
etgkqo <etgkqo@gmail.com>
2022-11-01 20:39:35 +0900

    MessageIdGenerator -> MessageIdHelper 변경으로 인한 수정

--- app/src/main/java/com/coinlive/chat/firebase/model/Chat.kt
+++ app/src/main/java/com/coinlive/chat/firebase/model/Chat.kt
@@ -2,13 +2,13 @@ package com.coinlive.chat.firebase.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.coinlive.chat.util.MessageIdGenerator
+import com.coinlive.chat.util.MessageIdHelper
 import com.google.firebase.Timestamp
 import com.google.gson.annotations.SerializedName
 
 @Entity
 data class Chat(
-    @PrimaryKey val messageId:String = MessageIdGenerator.generateMessageId(),
+    @PrimaryKey val messageId:String = MessageIdHelper.generateMessageId(),
     @SerializedName("sym") val symbol:String,
     @SerializedName("aid") val firebaseAuthId:String?= null,
     @SerializedName("cid") val coinId:String,
@@ -29,8 +29,8 @@ data class Chat(
     @SerializedName("rp_men") val replyEnMessage:String?= null,
     @SerializedName("rp_mid") val replyUserId:String?= null,
     @SerializedName("lab") val label:String?= null,
-    @SerializedName("emo") val emoji:Map<String, Emoji>?= null,
-    @SerializedName("img") val images:List<String>?= null,
+    @SerializedName("emo") val emoji:HashMap<String, Emoji>?= null,
+    @SerializedName("img") val images:ArrayList<String>?= null,
     @SerializedName("isHolder") val isHolder:Boolean = false,
     @SerializedName("asset") val asset: Asset?= null,
     @SerializedName("st") val st:Timestamp?= null
