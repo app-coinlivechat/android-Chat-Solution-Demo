ae36a4a83cfc0d1e504ecaa866dd5bc4cbd02c7b
etgkqo <etgkqo@gmail.com>
2022-11-01 20:38:51 +0900

    getMidnightTimeStampByMillis 구현 및 getTodayMidnight 수정

--- app/src/main/java/com/coinlive/chat/util/CalendarHelper.kt
+++ app/src/main/java/com/coinlive/chat/util/CalendarHelper.kt
@@ -31,11 +31,18 @@ object CalendarHelper {
      * UTC 기준 오늘 자정 [Calendar] 를 전달합니다.
      */
     fun getTodayMidnight(): Calendar {
-        val now = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
-        val tempTime = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
-        tempTime.clear()
-        tempTime.set(now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DATE), 0, 0, 0)
-        return tempTime
+        val now = nowCalendar()
+        now.set(now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DATE), 0, 0, 0)
+        now.set(Calendar.MILLISECOND,0)
+        return now
+    }
+
+    fun getMidnightTimeStampByMillis(millis : Long) : Long {
+        val empty = nowCalendar()
+        empty.timeInMillis = millis
+        empty.set(empty.get(Calendar.YEAR), empty.get(Calendar.MONTH), empty.get(Calendar.DATE), 0, 0, 0)
+        empty.set(Calendar.MILLISECOND,0)
+        return empty.timeInMillis
     }
 
     /**
