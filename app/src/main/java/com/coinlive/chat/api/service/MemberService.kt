db22dbc62d7b870e8ba207a6570563836425f298
etgkqo <etgkqo@gmail.com>
2022-11-03 11:04:03 +0900

    사용하지 않는 주석 삭제

--- app/src/main/java/com/coinlive/chat/api/service/MemberService.kt
+++ app/src/main/java/com/coinlive/chat/api/service/MemberService.kt
@@ -5,18 +5,6 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface MemberService {
-    /*
-    /v1/member/check/nickname [GET] // 닉네임 설정 여부
-    /v1/member/nickname [PUT] // 닉네임 수정 {auth}
-    /v1/member/check [POST] // 멤버 확인
-    회원탈퇴 -> 운영으로 처리
-    /v1/member/{mid}/info [GET] {auth} // mid 회원 정보 조회
-    /v1/member/profile/basic [PUT] {auth} // 유저 프로필 기본 이미지 설정
-    /v1/member/report/type [GET] {auth} // 신고 타입 조회
-    /v1/member/report [POST] {auth} // 유저 신고
-    /v1/member/{bMid}/block [Delete, post] {auth} //유저 차단 추가,삭제
-     */
-
     @GET("v1/member/check/nickname")
     fun isAvailableNickName(@Body nickName: NickNameBody): Call<RestApiResponse<NickName>>
 
