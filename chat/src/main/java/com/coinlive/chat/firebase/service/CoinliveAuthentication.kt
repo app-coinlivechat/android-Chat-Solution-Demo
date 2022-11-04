43580e34162e763e3807a71c6afc996e686b9246
etgkqo <etgkqo@gmail.com>
2022-11-04 14:52:55 +0900

    signIn,getFirebaseToken suspend function으로 변경

--- chat/src/main/java/com/coinlive/chat/firebase/service/CoinliveAuthentication.kt
+++ chat/src/main/java/com/coinlive/chat/firebase/service/CoinliveAuthentication.kt
@@ -20,7 +20,7 @@ object CoinliveAuthentication {
      * @return[FirebaseUser] 사용자의 User[FirebaseUser] object를 리턴합니다.
      * @see ChattingMemberRepository.getCustomToken
      */
-    suspend fun signIn(customToken: String): FirebaseUser {
+    suspend fun signIn(customToken: String): FirebaseUser{
         try {
             Firebase.auth.signInWithCustomToken(customToken).await()
             return Firebase.auth.currentUser ?: throw FirebaseIdTokenException("CoinliveAuthentication.signIn error")
@@ -45,16 +45,16 @@ object CoinliveAuthentication {
      * 이 function을 사용하기 위해서는 [signIn]이 선행되어야 합니다.
      * @return[String] 현재 Firebase Authentication에 로그인된 유저의 IdToken을 전달합니다. 로그인이 되어있지 않다면 null이 전달됩니다.
      */
-    fun getFirebaseToken(): String {
+    suspend fun getFirebaseToken(): String {
         val currentUser: FirebaseUser =
             Firebase.auth.currentUser ?: throw FirebaseIdTokenException("CoinliveAuthentication.getFirebaseToken error")
         try {
-            return currentUser.getIdToken(true).result.token
+            val result = currentUser.getIdToken(true).await()
+            return result.token
                 ?: throw UnknwonExecption("CoinliveAuthentication.getFirebaseToken error" +
                         "error")
         } catch (e: Exception) {
             throw UnknwonExecption("CoinliveAuthentication.getFirebaseToken error")
-
         }
     }
 
