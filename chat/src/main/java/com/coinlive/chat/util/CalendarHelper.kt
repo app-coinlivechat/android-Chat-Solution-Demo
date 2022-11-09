ce514c62b811322d2e81e55e96369281f08db1ff
etgkqo <etgkqo@gmail.com>
2022-11-09 21:15:11 +0900

    chat : CalendarHelper 사용하지 않는 fun 삭제

--- chat/src/main/java/com/coinlive/chat/util/CalendarHelper.kt
+++ chat/src/main/java/com/coinlive/chat/util/CalendarHelper.kt
@@ -13,21 +13,6 @@ object CalendarHelper {
     }
 
     /**
-     * UTC 기준 어제 자정 타임스탬프를 전달합니다.
-     * @return[Long] 타임스탬프
-     */
-    fun getYesterdayMidnightTimeStamp(): Long {
-        val today = getTodayMidnight()
-        today.set(Calendar.DATE, today.get(Calendar.DATE) - 1)
-        return today.timeInMillis
-    }
-
-    fun getDayAgoMidnightTimeStamp(standardCalendar: Calendar,daysAgo:Int) : Long {
-        standardCalendar.set(Calendar.DATE, standardCalendar.get(Calendar.DATE) - daysAgo)
-        return standardCalendar.timeInMillis
-    }
-
-    /**
      * UTC 기준 오늘 자정 [Calendar] 를 전달합니다.
      */
     fun getTodayMidnight(): Calendar {
